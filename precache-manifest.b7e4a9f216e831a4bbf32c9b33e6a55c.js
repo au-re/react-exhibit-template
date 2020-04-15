3d9abae8e0e6c23a878ab8de372dd9fc4faa89a9
Aurelien <au.re@live.com>
2020-04-15 14:54:47 +0200

    Updates

---
+++ precache-manifest.b7e4a9f216e831a4bbf32c9b33e6a55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce9f02a720d565854b1e2656fd6c19e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d160899f30f835b19b4f.bundle.js"
+  },
+  {
+    "url": "runtime~main.d160899f30f835b19b4f.bundle.js"
+  },
+  {
+    "revision": "f4ae78db09b05bb4807e",
+    "url": "static/css/main.1aa19b0b.chunk.css"
+  },
+  {
+    "url": "vendors~main.d160899f30f835b19b4f.bundle.js"
+  },
+  {
+    "url": "vendors~main.d160899f30f835b19b4f.bundle.js.LICENSE.txt"
+  }
+]);
