b135bbd1a440fc4f912ae814f655a00f4a24aeb8
Aurélien <au.re@live.com>
2020-04-27 15:17:35 +0200

    :bug: add react-exhibit brand

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,6 +3,6 @@ import { create } from "@storybook/theming/create";
 export default create({
   base: "light",
   brandTitle: "react-exhibit-template",
-  brandUrl: "https://github.com/au-re/react-exhibit-template",
-  brandImage: "",
+  brandUrl: "https://github.com/au-re/generator-react-exhibit",
+  brandImage: "https://raw.githubusercontent.com/au-re/generator-react-exhibit/master/assets/icon.png",
 });
