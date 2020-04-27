eea2601b0e39735bf3a97128bf712583f7b1ce9c
Aurélien <au.re@live.com>
2020-04-27 15:23:55 +0200

    :bug: (storybook) update theme urls

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,7 +2,7 @@ import { create } from "@storybook/theming/create";
 
 export default create({
   base: "light",
-  brandTitle: "react-exhibit-template",
-  brandUrl: "https://github.com/au-re/react-exhibit-template",
-  brandImage: "https://raw.githubusercontent.com/au-re/typescript-lib-starter/master/assets/icon.png",
+  brandTitle: "<%= appName %>",
+  brandUrl: "<%= homepageUrl %>",
+  brandImage: "https://raw.githubusercontent.com/au-re/generator-react-exhibit/master/assets/icon.png",
 });
